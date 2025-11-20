4e74430039ab74dbeb63c069bbf6a892c513c444
Jaya <salehjaya97@gmail.com>
2025-11-20 20:57:25 +0700

    fix deploy add basic route

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,15 @@ import cors from 'cors';
 dotenv.config();
 
 const app = express();
-
-
 app.use(cors({
   origin:['http://localhost:5173'], credentials:true
 }))
+
 app.use(express.json());
 app.use("/api", router);
-
-
+// Basic route
+app.get('/', (req, res) => {
+  res.send('CRM PT. Smart is running!');
+});
 
 export default app;
